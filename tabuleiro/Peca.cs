00967f68d901a41d28f67f24a35aa5a54ab01543
Rodrigo Anjos <anjos.rodrigo@outlook.com.br>
2023-03-04 21:42:18 -0300

    Comecando a implementar tabuleiro

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QteMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.Cor = cor;
+            this.Posicao = posicao;
+            this.Tab = tab;
+            this.QteMovimentos = 0;
+
+        }
+    }
+}
