e6df2eed378ab8771691d711b86475f8aed947ae
Rodrigo Anjos <anjos.rodrigo@outlook.com.br>
2023-03-04 22:40:54 -0300

    Comecando a colocar pecas no tabuleiro

--- tabuleiro/Peca.cs
+++ tabuleiro/Peca.cs
@@ -10,10 +10,10 @@ namespace tabuleiro
         public int QteMovimentos { get; protected set; }
         public Tabuleiro Tab { get; protected set; }
 
-        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        public Peca(Tabuleiro tab, Cor cor)
         {
             this.Cor = cor;
-            this.Posicao = posicao;
+            this.Posicao = null;
             this.Tab = tab;
             this.QteMovimentos = 0;
 
--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -10,18 +10,24 @@ namespace tabuleiro
     {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
-        private Peca[,] Pecas { get; set; }
+        private Peca[,] pecas { get; set; }
 
         public Tabuleiro(int linhas, int colunas)
         {
             this.Linhas = linhas;
             this.Colunas = colunas;
-            Pecas = new Peca[Linhas, Colunas];
+            pecas = new Peca[Linhas, Colunas];
         }
 
         public Peca peca(int linha, int coluna)
         {
-            return Pecas[linha, coluna];
+            return pecas[linha, coluna];
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
         }
     }
 }
